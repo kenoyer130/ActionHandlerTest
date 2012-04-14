2b8298e6e68d2dae8628f8b221c6723614a2e39b
jkenoyer <kenoyer130@gmail.com>
2012-04-14 17:04:37 -0400

    work on table

--- mvc4/MvcWeb/Scripts/controls/appgrid.js
+++ mvc4/MvcWeb/Scripts/controls/appgrid.js
@@ -1,39 +1,101 @@
 ﻿(function ($) {
 
+    var cellRenderers;
+
+    function defaultCellRenderer(options) {
+        return "<td>" + options.obj[options.key] + "</td>";
+    };
+
     var methods = {
         init: function (options) {
             this.appgrid.settings = $.extend({}, this.appmenu.defaults, options);
+            cellRenderers = this.appgrid.cellRenderers;
+            methods["addCellRenderer"]({ id: "default", callback: defaultCellRenderer });
+        },
+
+        addCellRenderer: function (options) {
+            cellRenderers[options.id] = options.callback;
         },
 
         load: function (options) {
 
-                $.getjson(options.url, null, function (data) {
-                               
-                    var output = "";
-                    if (data == null)
-                        return;
+            base = this;
 
-                    var count = data.length;
+            if (!this.is("table"))
+                throw exception("you must call this plugin from a table element!");
 
-                    for (var i = 0; i < count; i++) { 
-                         output += output = "<tr>";
+            // load column headers
+            var headerTH = this.find("th");
+            if (headerTH == null)
+                throw exception("you need a th element in your table!");
 
-                        var jsize = data[i].length;
-                      
-                        for (var j = 0; j < jsize; j++) {
-                            output += output = "<td>" + data[i][j] + "</td>";
+            var tbody = this.find("tbody");
+            if (tbody == null)
+                throw exception("you need a tbody element in your table!");
+
+            var header = {}
+
+            // loop through and build our column array
+            $.each(headerTH, function (index, headerth) {
+                if (!headerth.attributes["class"]) {
+                    header[headerth.id] = "";
+                } else {
+                    header[headerth.id] = headerth.attributes["class"].value;
+                }
+            });
+
+            // call the indicated json rpc call
+            $.getJSON(this.appgrid.settings.basePath + options.url, null, function (data) {
+
+                var output = "";
+                if (data == null)
+                    return;
+
+                // create array of rows
+                var rows = new Array();
+
+                // iterate each returned data row
+                $.each(data, function (index, obj) {
+
+                    var row = new Array(header.length);
+
+                    // map the returned properties to the correct columns
+                    for (key in obj) {
+                        if (!obj.hasOwnProperty(key)) {
+                            continue;
                         }
 
-                        output += output = "</tr>";
+                        for (headerkey in header) {
+
+                            if (header[headerkey] in cellRenderers) {
+                                row[headerkey] = cellRenderers[header[headerkey]]({ key: key, obj: obj });
+                            }
+                            else {
+                                row[headerkey] = cellRenderers["default"]({ key: key, obj: obj });
+                            }
+                        }
                     }
 
-                    this.html(output);
+                    rows.push(row);
                 });
+
+
+                for (var i = 0; i < rows.length; i++) {
+                    output += "<tr>";
+
+                    for (key in rows[i]) {
+                        output += rows[i][key];
+                    }
+
+                    output += "</tr>";
+                }
+
+                tbody.html(output);
+            });
         }
     };
 
-    $.fn.appmenu = function (methodOrOptions) {
-
+    $.fn.appgrid = function (methodOrOptions) {
         if (methods[methodOrOptions]) {
             return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
@@ -45,9 +107,10 @@
     };
 
     $.fn.appgrid.defaults = {
-       
+        basePath: '/'
     };
 
     $.fn.appgrid.settings = {}
+    $.fn.appgrid.cellRenderers = {}
 
 })(jQuery);
